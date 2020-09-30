5e50278f702a7423caed7f5f22ac3c3cd1a238c3
Muhammad Riziq <muhriz14@gmail.com>
2020-09-30 14:50:25 +0700

    Pertemuan 1 + React

---
+++ Pertemuan 1/Script_3.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App(){
+    <div className="App">
+        <header className="App-header">
+            <p>Muhammad Riziq Yahya</p>
+            <a 
+            className="App-link"
+            href="https://github.com/gatdemit/CERC-Web/tree/master/Pertemuan%201" 
+            target="_blank"
+            rel="noopener noreferrer">
+                Link Git
+                </a>
+        </header>
+    </div>
+}
+
+export default App;
---
+++ Pertemuan 1/challenge-react/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Halo! Namaku Muhammad Riziq Yahya!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/gatdemit/CERC-Web/tree/master/Pertemuan%201"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Link GitHubku!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
